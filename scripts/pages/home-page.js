f5488a95d9a5ffd0b67012e74dc0336b24e73053
Alex Mamani <alex.mamani@tecsup.edu.pe>
2022-06-04 22:59:23 -0500

    feat: create Todo's and add listeners for completed and important task

--- scripts/pages/home-page.js
+++ scripts/pages/home-page.js
@@ -1,7 +1,11 @@
+import { input } from "../components/input.js";
+
 import DOMHandler from "../dom-handler.js";
 import STORE from "../store.js";
 import { logout } from "../services/sessions-service.js"
 import LoginPage from "./login-page.js";
+import { createTodo, updateTodo } from "../services/todos-services.js"
+
 
 function renderDo(toDos) {
   return `<br>
@@ -9,12 +13,12 @@ function renderDo(toDos) {
    `
   <div class="each--do">
     <div class="width-280">
-      <input type="checkbox" id="cb-complete" name="cb-complete" value="cbComplete" ${todo.completed? "checked":""}>
+      <input type="checkbox"  class="checkbox-completed" id="${todo.id}" data="${todo.id}" name="cb-complete" value="cbComplete" ${todo.completed? "checked":""}>
       <label for="cb-complete">  ${todo.title}</label><br>
       <span class="due-date"> ${todo.due_date}<span>
     </div>
     <div>  
-    <img src=${todo.completed&&todo.important? "assets/icons/important_soso.svg":
+    <img class="js-images" data-ref="${todo.id}" src=${todo.completed&&todo.important? "assets/icons/important_soso.svg":
     (todo.important?"assets/icons/important_red.svg":"assets/icons/important_gray.svg")
     }>
     </div> 
@@ -28,7 +32,7 @@ function renderDo(toDos) {
 
 function render() {
   const currenTab = STORE.currenTab
-
+  STORE.hoy = new Date()
   return `  
     <header>
       <a class="text-center block mb-8 js-logout">Logout</a>
@@ -71,18 +75,44 @@ function render() {
               <img src="assets/icons/important_red.svg">
             </div>  
 
-${STORE.showOnlyPending&&STORE.showOnlyImportant?renderDo(STORE.toDos_sorted.filter((x)=>x.completed==false).filter((x)=>x.important==true)):
-  STORE.showOnlyImportant?renderDo(STORE.toDos_sorted.filter((x)=>x.important==true)):
-  STORE.showOnlyPending?renderDo(STORE.toDos_sorted.filter((x)=>x.completed==false)):
-  renderDo(STORE.toDos_sorted)
-          
-          }
-              
+            ${STORE.showOnlyPending&&STORE.showOnlyImportant?renderDo(STORE.toDos_sorted.filter((x)=>x.completed==false).filter((x)=>x.important==true)):
+              STORE.showOnlyImportant?renderDo(STORE.toDos_sorted.filter((x)=>x.important==true)):
+              STORE.showOnlyPending?renderDo(STORE.toDos_sorted.filter((x)=>x.completed==false)):
+              renderDo(STORE.toDos_sorted)}
+          </div>
+          <br>
+
+          <form class="flex flex-column gap-4 mb-4 js-create-form">
+
+          ${input({
+            id: "title",
+            type: "text",
+            placeholder: "do the dishes...",
+            required: true,
+            value: ""
+          })}
+
+          ${input({
+            id: "due_date",
+            type: "date",
+            placeholder: "dd/mm/yyyy",
+            required: true,
+            value:  (STORE.hoy).getFullYear()+"-"+
+            ((STORE.hoy).getMonth()<10?"0"+(STORE.hoy).getMonth():+(STORE.hoy).getMonth())+"-"+
+            ((STORE.hoy).getDay()<10?"0"+(STORE.hoy).getDay():+(STORE.hoy).getDay())
+          })}
+
+
+
+          <button class="button button--primary ">Add Task</button>
+        </form>
+
+
+          <div>
+
 
           </div>
         </div>
-        </div>
-
       </section>
     </main>
   `;
@@ -91,6 +121,34 @@ ${STORE.showOnlyPending&&STORE.showOnlyImportant?renderDo(STORE.toDos_sorted.fil
 
 
 
+function listenSubmitForm(that) {
+  const form =  document.querySelector(".js-create-form")
+
+  form.addEventListener("submit", async (event) => {
+    try {
+      event.preventDefault();
+      console.dir(event.target)
+      const { title, due_date } = event.target;
+      console.log(title, due_date)
+      const valuesTodo = {
+        title: title.value,
+        due_date: due_date.value,
+      }
+  
+      let a = await createTodo(valuesTodo)
+      console.log(a)
+
+      await STORE.fetchToDos_sorted()
+      DOMHandler.load(HomePage)
+    } catch (error) {
+      
+      console.log(error)
+      that.state.loginError = error.message
+      LoginPage.state.loginError = error.message
+      DOMHandler.reload()
+    }
+  })
+}
 
 
 function listenSelect() {
@@ -141,13 +199,65 @@ function listenOnlyImportant() {
   })
 }
 
+{/* <div class="each--do">
+<div class="width-280">
+  <input type="checkbox" class="checkbox-completed" id="cb-complete" name="cb-complete" value="cbComplete" ${todo.completed? "checked":""}>
+  <label for="cb-complete">  ${todo.title}</label><br> */}
+
+//COMPLETED
+function listenCompletedClick() {
+  const checkboxesC = document.querySelectorAll(".checkbox-completed");
+  checkboxesC.forEach((checkboxC) => {
+    checkboxC.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.target.checked=!e.target.checked //ok
+      let checkboxId = +checkboxC.closest("input").id
+      let valuesTodo=STORE.toDos_sorted.filter((x)=>x.id==checkboxId)[0]
+      valuesTodo.completed=!valuesTodo.completed
+
+      updateTodo(checkboxId,valuesTodo)
+      try {
+        DOMHandler.reload(this);
+      } catch (error) {
+        console.log(error.message);
+      }
+
+    });
+  });
+}
+
+
+function listenImportantClick() {
+  const imagesImportant = document.querySelectorAll(".js-images");
+  imagesImportant.forEach((imageImportant) => {
+    imageImportant.addEventListener("click", (e) => {
+      e.preventDefault();
+      let imageId = e.target.dataset.ref
+      console.log(imageImportant.closest("img")) //html
+
+      let valuesTodo=STORE.toDos_sorted.filter((x)=>x.id==imageId)[0]
+      valuesTodo.important=!valuesTodo.important
+      console.log(valuesTodo) // JSON
+      updateTodo(imageId,valuesTodo)
+
+      try {
+        DOMHandler.reload(this);
+      } catch (error) {
+        console.log(error.message);
+      }
+
+    });
+  });
+}
+
+
 function listenLogout() {
   const a = document.querySelector(".js-logout")
   a.addEventListener("click", async (event) => {
     event.preventDefault();
     
     try {
-      await logout();
+      console.log(await logout());
       DOMHandler.load(LoginPage);
     } catch (error){
       console.log(error.message);
@@ -164,7 +274,10 @@ const HomePage = {
     listenSelect();
     listenOnlyPending();
     listenOnlyImportant();
-
+    listenSubmitForm();
+    listenCompletedClick();
+    listenImportantClick();
+    
   }
 }
 
